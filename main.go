b4bcccc38d137f613e0009121f1e49eb7110ee1e
Harry Muir Camacho <hmuir@Harrys-MacBook-Pro.local>
2023-12-10 21:46:55 -0600

    Added hardcoded endpoint to create user

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"log"
+
+	pb "github.com/hmuir28/go-grpc-user-management/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8000"
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewUserServiceClient(conn)
+
+	createUserRequest := &pb.CreateUserRequest{
+		FirstName: "Harry",
+		LastName:  "Muir",
+		Username:  "hmuir@gmail.com",
+		Password:  "12345",
+		Email:     "hmuir@gmail.com",
+	}
+
+	// callSayHello(client)
+	// callSayHelloServerStreaming(client, names)
+	// callSayHelloClientStreaming(client, names)
+	us.callCreateUser(client, createUserRequest)
+}
