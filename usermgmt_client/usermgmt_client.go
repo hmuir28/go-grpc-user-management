01bdb1cfe022aa8a363175952ec4b73db0441215
Harry Muir Camacho <hmuir@Harrys-MacBook-Pro.local>
2024-02-21 20:44:08 -0600

    Added Unary grpc to create user

---
+++ usermgmt_client/usermgmt_client.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/hmuir/go-user-management/usermgmt"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	var new_users = make(map[string]int32)
+
+	new_users["Alice"] = 32
+	new_users["Bob"] = 29
+	new_users["Harry"] = 12
+
+	for name, age := range new_users {
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{
+			Name: name,
+			Age:  age,
+		})
+
+		if err != nil {
+			log.Fatalf("could not create user: %v", err)
+			return
+		}
+
+		log.Printf(`
+			User Details:
+				NAME: %s
+				AGE: %d
+				ID: %d
+		`, r.GetName(), r.GetAge(), r.GetId())
+	}
+}
---
+++ usermgmt_server/usermgmt_server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+
+	pb "github.com/hmuir/go-user-management/usermgmt"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer
+}
+
+func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+
+	log.Printf("received: %v", in.GetName())
+
+	var user_id int32 = int32(rand.Intn(1000))
+
+	return &pb.User{
+		Name: in.GetName(),
+		Age:  in.GetAge(),
+		Id:   user_id,
+	}, nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserManagementServer(s, &UserManagementServer{})
+
+	log.Printf("server is listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
